211efb53d1e42e3d727eb90223e7f3f831e7a97a
unknown <mdsaikut@gmail.com>
2015-05-18 14:26:53 +0600

    BitmClass

---
+++ LopExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LopExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = 20;
+            //while (num < 25)
+            //{
+            //    Console.WriteLine("less than 25");
+            //    num += 1;
+            //}
+            //Console.ReadKey();
+
+
+            //do
+            //{
+            //    Console.WriteLine("less than 25");
+            //    num += 1;
+            //} while (num>25);
+            //Console.ReadKey();
+
+
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            //Console.ReadKey();
+
+
+            int sum = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+               
+                sum += i;
+               
+
+            }
+            Console.WriteLine("Total : {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
